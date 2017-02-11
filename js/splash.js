72a9d668acf14d2f5a9052271804c81e30a18357
金 鑫 <jinxin@hualala.com>
2017-02-11 15:42:18 +0800

    导航传参数
    导航传参数

---
+++ js/splash.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import  {
+	View,
+	Text,
+	TouchableOpacity
+} from 'react-native';
+import Login from './login';
+class Splash extends React.Component {
+	_openPage() {
+		this.props.navigator.push({
+			title: 'Login',
+			component: Login
+		})
+	}
+	render() {
+		return (
+			<View style={{ flex: 1, alignItems: 'center', backgroundColor: '#FFFFFF' }}>
+				<Text>Splash Page</Text>
+
+				<TouchableOpacity onPress={this._openPage.bind(this)}>
+					<Text style={{ color: '#55ACEE' }}>Open New Page</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+}
+
+export default Splash;
